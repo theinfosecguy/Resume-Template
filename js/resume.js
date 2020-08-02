f97ff55566782325ea51feb00997f765ee79149e
root <root@localhost.localdomain>
2020-08-02 23:30:15 +0530

    'Autocomplete Bars Fixed'

--- js/resume.js
+++ js/resume.js
@@ -23,49 +23,61 @@ for(var i=0;i<anchorTags.length;i++){
 
 // Auto Filling of the skills bar
 
-var progressBars = document.querySelectorAll('.skills-progress > div');
-var skillsContainer = document.getElementById('skills-container');
-console.log(skillsContainer);
-console.log(progressBars);
+// var progressBars = document.querySelectorAll('.skills-progress > div');
+// var skillsContainer = document.getElementById('skills-container');
+// console.log(skillsContainer);
+// console.log(progressBars);
 
-window.addEventListener('scroll' , checkScroll);
-var animFire = true;
+// window.addEventListener('scroll' , checkScroll);
+// var animFire = true;
 
-var skillsCords = skillsContainer.getBoundingClientRect();
+// var skillsCords = skillsContainer.getBoundingClientRect();
 
-function initBars(){
-    for(var bar of progressBars){
-        bar.style.width = 0 + '%';
+// function initBars(){
+//     for(var bar of progressBars){
+//         bar.style.width = 0 + '%';
+//     }
+// }
+
+// initBars();
+
+// function checkScroll(){
+
+//     if(skillsCords.top < window.innerHeight && animFire){
+//         console.log('Fun aara bro ab');
+//         animFire = false;
+//         fillBars();
+//     }
+//     else if(skillsCords.top > window.innerHeight){
+//         animFire = true;
+//         initBars();
+//     }
+// }
+
+// function fillBars(){
+
+//     for(let bar of progressBars){
+//         let targetWidth = bar.getAttribute('data-bar-width');
+//         let currentWidth = 0;
+//         let skillsInterval = setInterval(function(){
+//             if(currentWidth>=targetWidth){
+//                 clearInterval(skillsInterval);
+//                 return;
+//             }
+//             currentWidth++;
+//             bar.style.width = currentWidth + '%';
+//         },5);
+//     }
+// }
+
+
+var scrolled = false
+$(window).scroll(function() {
+    if ($('#skills').is(':visible')) {
+        // do your special stuff here
+        var skillbar = $('.skills-percent');
+        var per = skillbar.attr('data-bar-width');
+        console.log(per)
+        skillbar.css("width",per+'%');
     }
-}
-
-initBars();
-
-function checkScroll(){
-
-    if(skillsCords.top < window.innerHeight && animFire){
-        console.log('Fun aara bro ab');
-        animFire = false;
-        fillBars();
-    }
-    else if(skillsCords.top > window.innerHeight){
-        animFire = true;
-        initBars();
-    }
-}
-
-function fillBars(){
-
-    for(let bar of progressBars){
-        let targetWidth = bar.getAttribute('data-bar-width');
-        let currentWidth = 0;
-        let skillsInterval = setInterval(function(){
-            if(currentWidth>=targetWidth){
-                clearInterval(skillsInterval);
-                return;
-            }
-            currentWidth++;
-            bar.style.width = currentWidth + '%';
-        },5);
-    }
-}
+});
