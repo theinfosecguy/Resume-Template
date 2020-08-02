dd57347f6516673cc974fbba5db2fcc503150e3b
root <root@localhost.localdomain>
2020-08-03 00:12:51 +0530

    'Smooth Scroll Fixed'

--- js/resume.js
+++ js/resume.js
@@ -1,28 +1,29 @@
 //  For Smooth Scroll
+//  Smooth Scroll JS Added for Better Performance
 
-var anchorTags = document.querySelectorAll('nav a');
-// console.log(anchorTags);
+// var anchorTags = document.querySelectorAll('nav a');
+// // console.log(anchorTags);
 
-for(var i=0;i<anchorTags.length;i++){
-    // console.log(anchorTags[i]);
-    anchorTags[i].addEventListener('click' , function(event){
-        event.preventDefault();
-        var sectionId = this.textContent.trim().toLowerCase();
-        var sectionName = document.getElementById(sectionId);
-        console.log(sectionName);
-        var scrollInterval = setInterval(function(){
-            var cords = sectionName.getBoundingClientRect();
-            if(cords.top<=0){
-                clearInterval(scrollInterval);
-                return;
-            }
-            window.scrollBy(0,100);
-        },50);
-    });
-}
+// for(var i=0;i<anchorTags.length;i++){
+//     // console.log(anchorTags[i]);
+//     anchorTags[i].addEventListener('click' , function(event){
+//         event.preventDefault();
+//         var sectionId = this.textContent.trim().toLowerCase();
+//         var sectionName = document.getElementById(sectionId);
+//         console.log(sectionName);
+//         var scrollInterval = setInterval(function(){
+//             var cords = sectionName.getBoundingClientRect();
+//             if(cords.top<=0){
+//                 clearInterval(scrollInterval);
+//                 return;
+//             }
+//             window.scrollBy(0,100);
+//         },50);
+//     });
+// }
 
 // Auto Filling of the skills bar
-
+// Added Auto Fill Bars via jQuery
 // var progressBars = document.querySelectorAll('.skills-progress > div');
 // var skillsContainer = document.getElementById('skills-container');
 // console.log(skillsContainer);
@@ -44,7 +45,6 @@ for(var i=0;i<anchorTags.length;i++){
 // function checkScroll(){
 
 //     if(skillsCords.top < window.innerHeight && animFire){
-//         console.log('Fun aara bro ab');
 //         animFire = false;
 //         fillBars();
 //     }
